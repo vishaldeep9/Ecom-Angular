d405ef1f50773f60449cd52ecf038903cec33f71
vishaldeep09 <09vishaldeep@gmail.com>
2024-09-12 20:08:55 +0530

    some bugs fixed

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -10,36 +10,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./cart-page.component.css'],
 })
 export class CartPageComponent {
-  carData:cart[]|undefined;
-  priceSummary:priceSummary={
+  carData: cart[] | undefined;
+  priceSummary: priceSummary = {
     price: 0,
     discount: 0,
     delivery: 0,
     tax: 0,
-    total: 0
+    total: 0,
+  };
+
+  constructor(
+    private _productService: ProductsService,
+    private router: Router
+  ) {}
+  ngOnInit() {
+this.loadDetails();
   }
 
-  constructor(private _productService: ProductsService,private router:Router) {}
-  ngOnInit() {
+  loadDetails(){
     this._productService.currentCart().subscribe((data) => {
       // console.log(data)
-      this.carData=data;
-      let price=0;
-      data.forEach((item)=>{
-       if(item.quantity){
-        price=price+(item.price*item.quantity);
-       }
-      })
-      this.priceSummary.price=price,
-     this.priceSummary.delivery=100,
-     this.priceSummary.discount=price/10,
-     this.priceSummary.tax=price/5,
-     this.priceSummary.total=price-(price/10)+(price/5)+100;
-     console.log("Total price :"+this.priceSummary.total);
-
+      this.carData = data;
+      let price = 0;
+      data.forEach((item) => {
+        if (item.quantity) {
+          price = price + item.price * item.quantity;
+        }
+      });
+      (this.priceSummary.price = price),
+        (this.priceSummary.delivery = 100),
+        (this.priceSummary.discount = price / 10),
+        (this.priceSummary.tax = price / 5),
+        (this.priceSummary.total = price - price / 10 + price / 5 + 100);
+        //if cart data has no items  & undefined 
+        if(!this.carData.length){
+          this.router.navigate(['/']);
+        }
     });
   }
-  checkout(){
-this.router.navigate(['/checkout'])
+
+  checkout() {
+    this.router.navigate(['/checkout']);
+  }
+
+  removeToCart(cartId:string|undefined){
+    cartId && this._productService.removeToCartById(cartId).subscribe((result)=>{
+      this.loadDetails();
+    })
   }
 }
